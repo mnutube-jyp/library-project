ee8251a749d16698832bb13ecc46ac1bd489ae98
song160 <85055818+song160@users.noreply.github.com>
2021-12-01 13:45:07 +0900

    Modify popup
    Modify popup

--- Assets/Resources/Popup/PopupButton.cs
+++ Assets/Resources/Popup/PopupButton.cs
@@ -8,22 +8,11 @@ public class PopupButton : MonoBehaviour
     private GameObject target = null;
     private CallbackEvent callbackEvent = null;
 
-    private Vector3 scale = new Vector3(0, 0, 0);
-    private bool closeState = false;
-
     public void Init(string _text, CallbackEvent _callback, GameObject _target)
     {
         this.buttonString.text = _text;
         this.callbackEvent = _callback;
         this.target = _target;
-    }
-
-    private void Update()
-    {
-        if (closeState)
-        {
-            target.transform.localScale = Vector3.Lerp(transform.localScale, scale, Time.deltaTime * 10.0f);
-        }
     }
 
     public void OnButton()
