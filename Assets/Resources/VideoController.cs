6df45a57427243e2d8aa3c9b7f2b5aa7c328397e
jyp <jyp@mnutube.com>
2021-12-02 15:08:21 +0900

    Modify video player
    Modify video player

---
+++ Assets/Resources/VideoController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    [SerializeField]
+    private VideoPlayer videoPlayer = null;
+
+    [SerializeField]
+    private GameObject canvus = null;
+
+    void Start()
+    {
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "content.mp4");
+        Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey && videoPlayer.isPlaying == false)
+        {
+            Play();
+        }
+
+        if (canvus.transform.childCount >= 1)
+        {
+            SetVolumeMax();
+        }
+        else
+        {
+            SetVolumeMin();
+        }
+    }
+
+    void Play()
+    {
+        videoPlayer.Play();
+        videoPlayer.isLooping = true;
+    }
+
+    void SetVolumeMax()
+    {
+        videoPlayer.SetDirectAudioMute(0, false);
+        videoPlayer.SetDirectAudioVolume(0, 1);
+    }
+
+    void SetVolumeMin()
+    {
+        videoPlayer.SetDirectAudioMute(0, true);
+        videoPlayer.SetDirectAudioVolume(0, 0);
+    }
+}
